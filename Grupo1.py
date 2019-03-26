d83325e7531594532827eb9d647adb0beb5bab03
Tankraul <tankraul@gmail.com>
2019-03-26 19:52:07 -0300

    agregue mas texto al codigo

--- Grupo1.py
+++ Grupo1.py
@@ -1,7 +1,9 @@
 text = input ("Ingrese un texto: ")
 print (text)
+text = input ("presionar tecla para salir")
 
 Agregamos un texto.
 Agrego algo para crear conflicto.
 
 Agregamos texto GRUPO 3. 
+agregue una linea de salida

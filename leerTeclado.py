2a4fc075a1c66351fbdd54d714514163a4fe2c6f
gonzaFac <gonzaloperessi@gmail.com>
2019-03-26 20:37:52 -0300

    cambio realizados presione tecla espere2

--- leerTeclado.py
+++ leerTeclado.py
@@ -1,10 +1,6 @@
 
 import msvcrt
 print("Presione una tecla para continuar...")
-msvcrt.getch()
-
-
 tx=input("ingrese texto")
 print(tx)
-print("Presione una tecla para continuar...")
 msvcrt.getch()
